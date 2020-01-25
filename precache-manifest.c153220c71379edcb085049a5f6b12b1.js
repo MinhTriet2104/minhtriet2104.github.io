6ec646eedfcea11d485bd9c8f836406c48a25df1
minhtriet2104 <minhtriet2104.it@gmail.com>
2020-01-25 13:10:27 +0700

    Updates

---
+++ precache-manifest.c153220c71379edcb085049a5f6b12b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "402cb5aae9ea3d4949caebd21117fb25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "667466677b248105f4cb",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "35bca044bc2bd1e4632c",
+    "url": "/static/css/main.deec650d.chunk.css"
+  },
+  {
+    "revision": "667466677b248105f4cb",
+    "url": "/static/js/2.c94ecdad.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.c94ecdad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35bca044bc2bd1e4632c",
+    "url": "/static/js/main.fdcba0e5.chunk.js"
+  },
+  {
+    "revision": "0635208a32547d1f6d37",
+    "url": "/static/js/runtime-main.74fcc895.js"
+  },
+  {
+    "revision": "aaf74bc9b2fe5749f757e0e888459faa",
+    "url": "/static/media/IMG_9881.aaf74bc9.JPG"
+  }
+]);
